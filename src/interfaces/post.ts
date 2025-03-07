d5b9b8b0ccd8b01309a4a0bae273eea2a2237c08
alban <alban.blanchard@gmail.com>
2025-03-07 18:46:03 +0100

    post interface

---
+++ src/interfaces/post.ts
@@ -0,0 +1,32 @@
+
+export interface Post {
+	id: number;
+	title: { rendered: string };
+	excerpt: { rendered: string, protected: boolean };
+	content: { rendered: string, protected: boolean };
+	categories: number[];
+	tags: number[];
+	date: string;
+	featured_media: number;
+	_links: { 
+		'acf:term': {
+			href: string;
+			embeddable: boolean;
+			taxonomy: string;
+		}[];
+	};
+	acf: Acf;
+}
+
+interface Acf {
+	frontend_stack: number[];
+	backend_stack: number[];
+	devops_stack: number[];
+	languages: number[];
+	achievements: {
+		tech_achievements: string;
+		design_achievements: string;
+		other_achievements: string;
+	}
+	[key: string]: unknown;
+}
