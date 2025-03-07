d75bc1f83cac278cbd15ed54967e42abd44e777c
alban <alban.blanchard@gmail.com>
2025-03-07 18:32:47 +0100

    node conf for self signed cert in dev env

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+// https://docs.astro.build/en/guides/cms/umbraco/#local-dev-https-and-self-signed-ssl-certificates
+import { loadEnv } from "vite";
+const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
