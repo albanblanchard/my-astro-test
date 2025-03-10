bba3266ddce23a9b1061d3cc6bf6e429167cc7ee
alban <alban.blanchard@gmail.com>
2025-03-10 10:59:26 +0100

    node config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://docs.astro.build/en/guides/cms/umbraco/#local-dev-https-and-self-signed-ssl-certificates
 import { loadEnv } from "vite";
-const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 
 // https://astro.build/config
