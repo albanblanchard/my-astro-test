32c94bdd15454b7abfe1c32998287e22dd84cff9
alban <alban.blanchard@gmail.com>
2025-03-10 15:17:39 +0100

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,16 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://docs.astro.build/en/guides/cms/umbraco/#local-dev-https-and-self-signed-ssl-certificates
 import { loadEnv } from "vite";
-const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
+const { SITE_URL, BASE_URL, NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
+
+// https://docs.astro.build/en/guides/cms/umbraco/#local-dev-https-and-self-signed-ssl-certificates
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://albanblanchard.github.io',
-  base: 'my-astro-test',
+  site: SITE_URL,
+  base: BASE_URL,  
   integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
