aa741d03c2339e93eb418c7ab1604430c224d5a3
alban <alban.blanchard@gmail.com>
2025-03-10 17:02:44 +0100

    test  public vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,15 +7,15 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 import { loadEnv } from "vite";
-const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
+const { PUBLIC_SITE_URL, PUBLIC_BASE_URL, NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
 
 // https://docs.astro.build/en/guides/cms/umbraco/#local-dev-https-and-self-signed-ssl-certificates
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
-  base: 'my-astro-test',  
+  site: PUBLIC_SITE_URL,
+  base: PUBLIC_BASE_URL,  
   integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
